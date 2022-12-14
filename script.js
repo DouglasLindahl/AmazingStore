24c0f91069ae3ffe452a29ede9e49f0fe7a99414
Robin Persson <robinpn98@hotmail.se>
2022-12-14 11:52:15 +0100

    added menu animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".burgerContainer");
+const burger = document.querySelectorAll("burger");
 
 const body = document.querySelector("body");
 
@@ -8,6 +9,7 @@ const body = document.querySelector("body");
 
 menuButton.addEventListener("click", () => {
     menu.classList.toggle("menuOpen");
+    menuButton.classList.toggle("active");
 })
 
 window.addEventListener("scroll", () => {
